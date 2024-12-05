2b71525e2db8150d914a7d24aea15b2676fce332
akashps <itzmeakashps@gmail.com>
2024-12-05 15:24:08 +0530

    re chechking the validation for emirate ID

--- services/llm_services.py
+++ services/llm_services.py
@@ -1044,7 +1044,7 @@ def process_user_input(user_input: UserInput):
                         
                     ),
                     "question": f"Let’s try again: {question}",
-                    "example": f" Here's an example of a valid Emirates ID for reference: {emirates_id_example}. Please ensure it follows the format: 784-YYYY-XXXXXXX-X, where YYYY is your birth year, followed by a 7-digit number, and ending with a single digit."
+                    "example": f"Here's an example of a valid Emirates ID for reference: {emirates_id_example}. Please ensure it follows the format: 784-YYYY-XXXXXXX-X, where YYYY is your birth year, followed by a 7-digit number, and ending with a single digit."
                 }
        
 
