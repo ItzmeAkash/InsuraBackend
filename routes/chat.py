ad4d8b89b9823f6b3172dcdaceb7d1933f52a723
ItzmeAkash <itzmeakashps@gmail.com>
2025-09-10 15:10:32 +0530

    updated it

--- routes/chat.py
+++ routes/chat.py
@@ -4,7 +4,7 @@ from models.model import UserInput
 from services.llm_services import process_user_input
 
 router = APIRouter()
-
+#Chat
 @router.post("/chat/")
 async def chat_with_bot(user_input: UserInput):
     return process_user_input(user_input)
